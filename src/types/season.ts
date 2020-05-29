cb42778899a7af01602d17a2a337e3bfee39d01d
Aqbal Ahmad <aqbal.ahmad@navyuginfo.com>
2020-05-29 10:42:24 +0530

    added season types

--- src/types/season.ts
+++ src/types/season.ts
@@ -1,6 +1,29 @@
+export enum SeasonName {
+  SUMMER = 'Summer',
+  WINTER = 'Winter'
+}
 
 export interface Season {
   seasonId: number;
-  seasonName: string;
+  lseId: number;
   seasonGroupId: number;
+  seasonName: SeasonName;
+  seasonFromMonth: number;
+  seasonFromDay: number;
+  fromEdgePredominance: string;
+  seasonToMonth: number;
+  seasonToDay: number;
+  toEdgePredominance: string;
+}
+
+export interface SeasonGroup {
+  seasonGroupId: number;
+  seasons: Season[];
+}
+
+/**
+ * User Defined Type Guard for SeasonGroup
+ */
+export function isSeasonGroup(arg: SeasonGroup): arg is SeasonGroup {
+  return arg.seasonGroupId !== undefined;
 }

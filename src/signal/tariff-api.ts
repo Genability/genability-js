d1c36142ee47f2ed7026ed65cbac41ace6645cab
Aqbal Ahmad <aqbal.ahmad@navyuginfo.com>
2020-08-27 10:54:01 +0530

    added fields in tariff-api GetTariffRequest

--- src/signal/tariff-api.ts
+++ src/signal/tariff-api.ts
@@ -83,6 +83,7 @@ export class GetTariffRequest extends BasePagedRequest {
   public bundleRates?: boolean;
   public applicableRatesOnly?: boolean;
   public lookupVariableRates?: boolean;
+  public fields?: string;
 
   addParams(addParam: AddParamCallback): void {
     addParam('populateProperties', this.populateProperties);
@@ -93,6 +94,7 @@ export class GetTariffRequest extends BasePagedRequest {
     addParam('bundleRates', this.bundleRates);
     addParam('applicableRatesOnly', this.applicableRatesOnly);
     addParam('lookupVariableRates', this.lookupVariableRates)
+    addParam('fields', this.fields);
   }
 }
 

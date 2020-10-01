bcdbe7b2c72a07d2efef2f6994ef50e548e1a6a3
pulkit <pulkit.gupta@navyuginfo.com>
2020-10-01 12:16:10 +0530

    Increase testcase coverage for contract,credentials,client

---
+++ src/rest-client/client.test.ts
@@ -0,0 +1,35 @@
+import { 
+  RestApiClient, RestApiCredentials
+} from './client';
+
+const emptyApiCredentials: RestApiCredentials = {
+  appId: '',
+  appKey: '',
+}
+
+const jwtApiCredentials: RestApiCredentials = {
+  jwt: ''
+}
+
+const proxyApiCredentials: RestApiCredentials = {
+  proxyReq: (a = "") => {return a}
+}
+
+class TestClass extends RestApiClient{
+
+}
+
+describe("Check api credentials", () => {
+  it("is Empty credentials", () => {
+    const obj: TestClass = new TestClass('', emptyApiCredentials);
+    expect(obj).toBeTruthy();
+  })
+  it("is jwt credentials", () => {
+    const obj: TestClass = new TestClass('', jwtApiCredentials);
+    expect(obj).toBeTruthy();
+  })
+  it("is proxy credentials", () => {
+    const obj: TestClass = new TestClass('', proxyApiCredentials);
+    expect(obj).toBeTruthy();
+  })
+})

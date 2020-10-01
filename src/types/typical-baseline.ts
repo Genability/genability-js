9add06a058ea6ea1bc6735307aead11c0e70f461
pulkit <pulkit.gupta@navyuginfo.com>
2020-10-01 11:47:43 +0530

    Fix breaking Testcases due to Taiff type changes

--- src/types/typical-baseline.ts
+++ src/types/typical-baseline.ts
@@ -87,33 +87,33 @@ export function suitableTypicalBuildingIdForTariff(arg: Tariff): string {
     if (!arg.minMonthlyConsumption && !arg.maxMonthlyConsumption && !arg.minMonthlyDemand && !arg.maxMonthlyDemand) {
       correspondingString = 'MEDIUM_COMMERCIAL';
     } else if (
-      arg.maxMonthlyConsumption <= 10000 ||
-      arg.maxMonthlyDemand <= 200
+      (arg.maxMonthlyConsumption && arg.maxMonthlyConsumption <= 10000) ||
+      (arg.maxMonthlyDemand && arg.maxMonthlyDemand <= 200)
     ) {
       correspondingString = 'SMALL_COMMERCIAL';
     } else if (
-      arg.maxMonthlyConsumption <= 50000 ||
-      arg.maxMonthlyDemand <= 400
+      (arg.maxMonthlyConsumption && arg.maxMonthlyConsumption <= 50000) ||
+      (arg.maxMonthlyDemand && arg.maxMonthlyDemand <= 400)
     ) {
       correspondingString = 'MEDIUM_COMMERCIAL';
     } else if (
-      arg.maxMonthlyConsumption > 50000 ||
-      arg.maxMonthlyDemand > 400
+      (arg.maxMonthlyConsumption && arg.maxMonthlyConsumption > 50000) ||
+      (arg.maxMonthlyDemand && arg.maxMonthlyDemand > 400)
     ) {
       correspondingString = 'LARGE_COMMERCIAL';
     } else if (
-      arg.minMonthlyConsumption >= 250000 ||
-      arg.minMonthlyDemand >= 500
+      (arg.minMonthlyConsumption && arg.minMonthlyConsumption >= 250000) ||
+      (arg.minMonthlyDemand && arg.minMonthlyDemand >= 500)
     ) {
       correspondingString = 'LARGE_COMMERCIAL';
     } else if (
-      arg.minMonthlyConsumption >= 50000 ||
-      arg.minMonthlyDemand >= 400
+      (arg.minMonthlyConsumption && arg.minMonthlyConsumption >= 50000) ||
+      (arg.minMonthlyDemand && arg.minMonthlyDemand >= 400)
     ) {
       correspondingString = 'MEDIUM_COMMERCIAL';
     } else if (
-      arg.minMonthlyConsumption < 50000 ||
-      arg.minMonthlyDemand < 400
+      (arg.minMonthlyConsumption && arg.minMonthlyConsumption < 50000) ||
+      (arg.minMonthlyDemand && arg.minMonthlyDemand < 400)
     ) {
       correspondingString = 'SMALL_COMMERCIAL';
     } 

04b4a9b98dbebac97daf97690e3ee0bc529ac9e8
Faiz Ahmad <faiz.ahmad@navyuginfo.com>
2020-10-15 10:42:42 +0530

    export contract file from sdk which includes Fields

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { Genability } from './genability'
 import * as types from './types';
 import * as restApis from './api';
 import * as credentials from './rest-client/credentials';
-
+import * as contract from "./rest-client/contract";
 const world = 'World';
 
 export function echoHello(word: string = world): string {
@@ -14,4 +14,4 @@ export const Client = Genability;
 export { types };
 export { restApis };
 export { credentials };
-
+export { contract };

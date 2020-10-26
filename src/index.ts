ab696a6ffd80e64cb76d559bacda3df30813fd22
Faiz Ahmad <faiz.ahmad@navyuginfo.com>
2020-10-26 18:13:57 +0530

    export restClient

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Genability } from './genability'
 import * as types from './types';
 import * as restApis from './api';
+import * as restClient from "./rest-client";
 import * as credentials from './rest-client/credentials';
 import * as contract from "./rest-client/contract";
 import { TariffRateFactory } from './util/tariff-rate-factory';
@@ -15,6 +16,7 @@ export { Genability };
 export const Client = Genability;
 export { types };
 export { restApis };
+export { restClient };
 export { credentials };
 export { contract };
 export { TariffRateFactory };

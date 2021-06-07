d783e333909f803a0361f7d3068b1d715a83c0a7
rchristy <rchristy@genability.com>
2021-06-07 15:48:27 -0700

    GEN-1893 copy formula property parsing logic from java code

---
+++ src/util/formula.test.ts
@@ -0,0 +1,41 @@
+import { Formula } from "./formula";
+
+describe('Test getProperties function', () => {
+  it('should return all non-function properties from formula with properties', () => {
+    const formula = '#fnMaxUpperLimit ( { #billingDemand + ( #excessKVAR406 / 20 ) , 0 } )';
+
+    const properties = Formula.getProperties(formula);
+
+    expect(properties).toBeTruthy();
+    expect(properties.size).toEqual(2);
+    expect(properties).toContain('billingDemand');
+    expect(properties).toContain('excessKVAR406');
+  });
+
+  it('should return empty list from formula with function and without properties', () => {
+    const formula = '#fnMin ( { 100 , 200 } )';
+
+    const properties = Formula.getProperties(formula);
+
+    expect(properties).toBeTruthy();
+    expect(properties.size).toEqual(0);
+  });
+
+  it('should return empty list from formula without properties', () => {
+    const formula = '100 + 200 / 50';
+
+    const properties = Formula.getProperties(formula);
+
+    expect(properties).toBeTruthy();
+    expect(properties.size).toEqual(0);
+  });
+
+  it('should return empty list from empty formula', () => {
+    const formula = '';
+
+    const properties = Formula.getProperties(formula);
+
+    expect(properties).toBeTruthy();
+    expect(properties.size).toEqual(0);
+  });
+})
---
+++ src/util/formula.ts
@@ -0,0 +1,50 @@
+export class Formula {
+  // All variables are prefixed with this
+  private static readonly VARIABLE_PREFIX = '#';
+
+  // All functions are prefixed with this, after the variable prefix e.g. the formulaString would be '#fnMin'
+  private static readonly FUNCTION_PREFIX = 'fn';
+
+  private static readonly EMPTY = '';
+
+  private static readonly REGEX_NULL_SAFE = '\\?';
+
+
+  public static getProperties(
+    formula: string
+  ): Set<string> {
+    //
+    // if there are no legitimate properties do not evaluate
+    //
+    if (!formula || !formula.includes('#')) {
+      return new Set();
+    }
+
+    const properties: Set<string> = new Set<string>();
+    const initialSplit = formula.substr(formula.indexOf(this.VARIABLE_PREFIX)).split(this.VARIABLE_PREFIX);
+
+    for (const str of initialSplit) {
+      if (!str || str.startsWith(this.FUNCTION_PREFIX)) {
+        continue;
+      }
+
+      // We use a space to separate properties from operators, numbers
+      // and other pieces of the formula. e.g. (#demand / 2) + 100 => #demand
+      const secondSplit = str.split(' ');
+
+      // Some commas can be in the formula if not spaced correctly.
+      let cleanProperty = secondSplit[0].replace(',', this.EMPTY);
+
+      // The ? denotes handling of nulls, so for purposes
+      // of determining the formulas, we ignore the ? characters.
+      cleanProperty = cleanProperty.replace(this.REGEX_NULL_SAFE, this.EMPTY);
+
+      properties.add(cleanProperty);
+    }
+
+    // Note: At this point in the corresponding Java code we register all of the function variables into
+    // the StandardEvaluationContext, but that is pretty meaningless in this function
+
+    return properties;
+  }
+}
